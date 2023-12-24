b943609e182162bfc16cc77b20ab1d6e4bc4db36
CevaPan <154664019+CevaPan@users.noreply.github.com>
2023-12-24 07:51:18 +0300

    main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -8,17 +8,20 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <iostream>
 using namespace std;
 
-//без break
-
+//switch пример
 int main()
 {
     int x;
     cin >> x;
     switch (x)
     {
-        case 1: case 2: case 3: case 4:
-        case 5: cout << "pабочий день"; 
-        case 6:  case 7: cout << "выходной";
+        case 1: cout << "понедельник"; break;
+        case 2: cout << "вторник"; break;
+        case 3: cout << "среда"; break;
+        case 4: cout << "четверг"; break;
+        case 5: cout << "пятница"; break;
+        case 6: cout << "суббота"; break;
+        case 7: cout << "воскресенье"; break;
         default: cout << "вы ошиблись";
     }
     return 0;
